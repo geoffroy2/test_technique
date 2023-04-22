af897df0e261fea88993a40d6e53a8eed98c42dd
geoffroy2 <geoffroyfleurylebig@gmail.com>
2023-04-22 06:24:50 +0000

    first commit

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RequestCreditController ;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// crée une demande
+Route::post("requestCredits/create",[ RequestCreditController::class ,'store']);
+
+//liste des demandes
+Route::get("requestCredits",[ RequestCreditController::class ,'findAll'])->middleware('auth.jwt');
+
+// register and login
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+
+    return $request->user();
+});
+
